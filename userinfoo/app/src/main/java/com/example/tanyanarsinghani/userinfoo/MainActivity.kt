f85df486b9647f427eaa92e628e4401903bd3726
Tanya Narsinghani <tanyanarsinghani01@gmail.com>
2019-02-19 12:25:20 +0530

    First Commit

---
+++ userinfoo/app/src/main/java/com/example/tanyanarsinghani/userinfoo/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.tanyanarsinghani.userinfoo
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import android.provider.MediaStore
+import android.net.Uri
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+    val Tag = "Activity 1"
+    private var btn: Button? = null
+    private var imageview: ImageView? = null
+    private val GALLERY = 1
+    private val CAMERA = 2
+    val PICK_IMAGE_REQUEST = 1
+    var uri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        togetpic.setOnClickListener {
+            val intent = Intent()
+
+            intent.type = "image/*";
+            intent.action = Intent.ACTION_GET_CONTENT;
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+        }
+
+        tonextactivity.setOnClickListener {
+            Log.d(Tag, "onCreate")
+            val msg: String = etname.text.toString()
+            val msg1: String = etcontact.text.toString()
+            val msg2: String = etaddress.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_name", msg)
+            intent.putExtra("user_contact", msg1)
+            intent.putExtra("user_address", msg2)
+            intent.putExtra("uri_extra",uri)
+            startActivity(intent)
+        }
+
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        if (requestCode === PICK_IMAGE_REQUEST && resultCode === android.app.Activity.RESULT_OK && intent?.data != null) {
+
+            uri = intent?.data
+
+
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                val imageView = findViewById<View>(R.id.userpicc) as ImageView
+                imageView.setImageBitmap(bitmap)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+
+        }
+
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(Tag, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(Tag, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(Tag, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(Tag, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(Tag, "onDestroy")
+    }
+
+}
---
+++ userinfoo/app/src/main/java/com/example/tanyanarsinghani/userinfoo/second_activity.kt
@@ -0,0 +1,54 @@
+package com.example.tanyanarsinghani.userinfoo
+
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_second.*
+
+
+class SecondActivity : AppCompatActivity() {
+
+    val Tag = "Activity 2"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(Tag, "onCreate")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val bundle: Bundle? = intent.extras
+        val msgg = bundle!!.getString("user_name")
+        val msgg1 = bundle.getString("user_contact")
+        val msgg2 = bundle.getString("user_address")
+        val m = bundle.getParcelable<Uri>("uri_extra")
+        tvNAME.text = msgg
+        tvCONTACT.text = msgg1
+        tvADDRESS.text = msgg2
+        userpicc.setImageURI(m)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(Tag, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(Tag, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(Tag, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(Tag, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(Tag, "onDestroy")
+    }
+}
+
